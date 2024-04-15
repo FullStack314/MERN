ec691656839f3e78c81a333a293ebd46b9a1db25
aiXpertLab <aiXpertLab@gmail.com>
2024-04-15 00:20:47 -0400

    Mongo

--- express/index.js
+++ express/index.js
@@ -1,59 +1,103 @@
+const Note = require('./models/mongoose_model')
 const express = require('express')
 const app = express()
+
+app.use(express.static('dist'))
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
 const cors = require('cors')
-app.use(express.json())
 
 app.use(cors())
+app.use(express.json())
+app.use(requestLogger)
 
-let notes = [{id: 1,content: 'The decline has resulted in a 19 P/E ratio and a 4.35% dividend yield. Some analysts believe the bottom is in for UPS stock.',important: true},
-  {id: 2,content: 'MSFT has been giving out dividends and growing them for 19 consecutive years.',important: false},
-  {id: 3,content: 'As conflicted as our judgements might be about Israel’s conduct of its war in Gaza, there should be no ambiguity about our condemnation of Iran’s attack on Israel. ',important: true}]
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hello Express World!</h1>')
+  response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
-
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  
-  if (note) {response.json(note)
-  } else {response.status(404).end()}
-})
-
-const generateId = () => {
-  const maxId = notes.length > 0? Math.max(...notes.map(n => n.id)): 0
-  return maxId + 1
-}
-
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+})
+
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
 
   const note = {
     content: body.content,
-    important: Boolean(body.important) || false,
-    id: generateId(),
+    important: body.important,
   }
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
-
-const PORT = 3001
+const PORT = process.env.MONGODB_PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
